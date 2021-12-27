0f020a1bbe588d94782a3c1fce420d0c2a33df99
Greyson Elkins <greysonelkins@gmail.com>
2021-12-27 11:55:56 -0700

    Find index of last read message in thread #3

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,10 +1,24 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
 const Messages = (props) => {
   const { messages, otherUser, userId } = props;
+  const [finalReadMessage, setFinalReadMessage] = useState(-1) 
+
+  const findFinalReadMessage = useCallback(() => {
+    const finalRead = messages.reduce((finalRead, message) => {
+      if (message.senderId === userId && message.otherUserRead) finalRead = message.id
+      return finalRead
+    }, -1)
+    if (finalRead) setFinalReadMessage(finalRead)
+  }, [messages, userId])
+
+  useEffect(() => {
+    if (messages.length === 0) return
+    findFinalReadMessage()
+  }, [messages, findFinalReadMessage])
 
   return (
     <Box>
@@ -12,7 +26,10 @@ const Messages = (props) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
+          <>
+            <SenderBubble key={message.id} text={message.text} time={time} />
+            {message.id === finalReadMessage && <>read</>}
+          </>
         ) : (
           <OtherUserBubble key={message.id} text={message.text} time={time} otherUser={otherUser} />
         );
