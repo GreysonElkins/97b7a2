636fb6d7dca3903249939e74e378da3d10c2a5a7
Greyson Elkins <greysonelkins@gmail.com>
2021-12-28 13:37:36 -0700

    Move message sorting from client to server #1

--- server/routes/api/conversations.js
+++ server/routes/api/conversations.js
@@ -19,9 +19,9 @@ router.get("/", async (req, res, next) => {
         },
       },
       attributes: ["id"],
-      order: [[Message, "createdAt", "DESC"]],
+      order: [[Message, "createdAt", "ASC"]],
       include: [
-        { model: Message, order: ["createdAt", "DESC"] },
+        { model: Message, order: ["createdAt", "ASC"] },
         {
           model: User,
           as: "user1",
@@ -50,6 +50,9 @@ router.get("/", async (req, res, next) => {
     for (let i = 0; i < conversations.length; i++) {
       const convo = conversations[i];
       const convoJSON = convo.toJSON();
+      console.log(convoJSON)
+
+      // determine unreadData 
 
       // set a property "otherUser" so that frontend will have easier access
       if (convoJSON.user1) {
@@ -68,7 +71,8 @@ router.get("/", async (req, res, next) => {
       }
 
       // set properties for notification count and latest message preview
-      convoJSON.latestMessageText = convoJSON.messages[0].text;
+      const mostRecentMessage = convoJSON.messages.length - 1
+      convoJSON.latestMessageText = convoJSON.messages[mostRecentMessage].text;
       conversations[i] = convoJSON;
     }
 
