ed398b9eafd7821e82341338efb299fe82f80d7d
Greyson Elkins <greysonelkins@gmail.com>
2022-01-05 11:53:57 -0700

    Add bold latest message on unread #3

--- client/src/components/Sidebar/ChatContent.js
+++ client/src/components/Sidebar/ChatContent.js
@@ -4,40 +4,47 @@ import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "flex",
-    justifyContent: "space-between",
+    display: 'flex',
+    justifyContent: 'space-between',
     marginLeft: 20,
     flexGrow: 1,
   },
   username: {
-    fontWeight: "bold",
+    fontWeight: 'bold',
     letterSpacing: -0.2,
   },
   previewText: {
     fontSize: 12,
-    color: "#9CADC8",
+    color: (props) => (props.isUnread ? '' : '#9CADC8'),
     letterSpacing: -0.17,
+    fontWeight: (props) => (props.isUnread ? 'bold' : ''),
+
   },
-}));
+}))
 
 const ChatContent = (props) => {
-  const classes = useStyles();
-
   const { conversation } = props;
   const { latestMessageText, otherUser } = conversation;
+
+  const styleProps = {
+    isUnread: conversation.unread?.newReceivedCount > 0,
+  };
+
+  const classes = useStyles(styleProps);
 
   return (
     <Box className={classes.root}>
       <Box>
-        <Typography className={classes.username}>
-          {otherUser.username}
-        </Typography>
-        <Typography className={classes.previewText}>
+        <Typography className={classes.username}>{otherUser.username}</Typography>
+        <Typography
+          className={classes.previewText}
+          isUnread={conversation.unread?.newReceivedCount > 0}
+        >
           {latestMessageText}
         </Typography>
       </Box>
     </Box>
-  );
+  )
 };
 
 export default ChatContent;
